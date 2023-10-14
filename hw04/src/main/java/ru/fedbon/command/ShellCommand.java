ebd4e0a3e2a4115ef8d9569977ba0ab8312f91ab
fedbon <fbondarev1990@gmail.com>
2023-10-14 20:44:18 +0300

    hw04

--- hw04/src/main/java/ru/fedbon/command/ShellCommand.java
+++ hw04/src/main/java/ru/fedbon/command/ShellCommand.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import ru.fedbon.AppRunner;
+import ru.fedbon.domain.Result;
+import ru.fedbon.service.ResultService;
+import ru.fedbon.service.TestingService;
+import ru.fedbon.service.UserService;
 
 
 @ShellComponent
@@ -12,10 +16,30 @@ public class ShellCommand {
 
     private final AppRunner appRunner;
 
+    private final TestingService testingService;
 
-    @ShellMethod(value = "Start testing", key = {"start"})
+    private final ResultService resultService;
+
+    private final UserService userService;
+
+    @ShellMethod(value = "startTesting", key = {"start-app"})
     public void startTesting() {
         appRunner.run();
+    }
+
+    @ShellMethod(value = "executeDisplayHeader", key = {"display-header"})
+    public void executeDisplayHeader() {
+        testingService.displayHeader();
+    }
+
+    @ShellMethod(value = "executeProcessTesting", key = {"process-testing"})
+    public void executeProcessTesting() {
+        testingService.processTesting(new Result(userService.getUser()));
+    }
+
+    @ShellMethod(value = "executeDisplayResult", key = {"display-result"})
+    public void executeDisplayResult() {
+        resultService.displayResult((new Result(userService.getUser())));
     }
 
 }
